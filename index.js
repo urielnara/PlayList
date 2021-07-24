3d044d21ceec7bdcf63a76d5ea5e8b6b5fc532f6
Uriel Navarro <87386565+urielnara@users.noreply.github.com>
2021-07-24 15:32:50 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,147 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+
+<head>
+<div id="header">
+			<ul class="nav">
+				<li><a href="">Inicio</a></li>
+				<li><a href="">Otras Playlist</a>
+					<ul>
+						<li><a href="https://www.youtube.com/watch?v=0-7IHOXkiV8&list=RD0-7IHOXkiV8&start_radio=1">Playlist YouTube<img src="https://logodownload.org/wp-content/uploads/2014/10/youtube-logo-5-2.png" align="center" width="60" height="40" alt="yt" /></a></li>
+						<li><a href="https://open.spotify.com/playlist/1T51EsTaPQuElp8pgImlOV?si=70eb6236c11b49db">Playlist Spotify<img src="https://www.freepnglogos.com/uploads/spotify-logo-png/spotify-download-logo-30.png" align="center" width="40" height="40" alt="spotify" /></a></li>
+					</ul>
+				</li>
+			</ul>
+    </div>
+    <div id="1" class="f1">
+<img alt="playlist" src="https://creativemornings.com/assets/CM-Restart-v3.gif?w=850" height="250"
+  width="500" />
+</div>
+</head>
+</html>
+<table style="text-align: left; width: 100%;" border="1"
+ cellpadding="2" cellspacing="2">
+    <tr>
+      <td id="c" class="dod">Cancion</td>
+      <td id="cc" class="doe">Reproducir</td>
+    </tr>
+<tr>
+<td id="rev" class="p">Level Of Concern</td>
+<td><a href="https://www.youtube.com/watch?v=loOWKm8GW6A"><span class="blanco"></span><img src="https://media1.giphy.com/media/coBIzlrDq93doYaA9U/giphy.gif?cid=a267dfa328yx6jb6prtte94g2rrawphg0jfq6c1cmn6hegjd&rid=giphy.gif&ct=s&1625875200092" width="150" height="40" alt="play" /></a></td>
+</tr>
+
+<tr>
+<td>Chlorine</td>
+<td><a href="https://www.youtube.com/watch?v=eJnQBXmZ7Ek"><span class="blanco"></span>
+<img src="https://media1.giphy.com/media/coBIzlrDq93doYaA9U/giphy.gif?cid=a267dfa328yx6jb6prtte94g2rrawphg0jfq6c1cmn6hegjd&rid=giphy.gif&ct=s&1625875200092" width="150" height="40" alt="play"/></a></td>
+</tr>
+
+<tr>
+<td>Stressed Out</td>
+<td><a href="https://www.youtube.com/watch?v=pXRviuL6vMY"><span class="blanco"></span>
+<img src="https://media1.giphy.com/media/coBIzlrDq93doYaA9U/giphy.gif?cid=a267dfa328yx6jb6prtte94g2rrawphg0jfq6c1cmn6hegjd&rid=giphy.gif&ct=s&1625875200092" width="150" height="40" alt="play"/></a></td>
+</tr>
+
+<tr>
+<td>Ride</td>
+<td><a href="https://www.youtube.com/watch?v=Pw-0pbY9JeU"><span class="blanco"></span>
+<img src="https://media1.giphy.com/media/coBIzlrDq93doYaA9U/giphy.gif?cid=a267dfa328yx6jb6prtte94g2rrawphg0jfq6c1cmn6hegjd&rid=giphy.gif&ct=s&1625875200092" width="150" height="40" alt="play"/></a></td>
+</tr>
+
+<tr>
+<td>Heathens</td>
+<td><a href="https://www.youtube.com/watch?v=UprcpdwuwCg"><span class="blanco"></span>
+<img src="https://media1.giphy.com/media/coBIzlrDq93doYaA9U/giphy.gif?cid=a267dfa328yx6jb6prtte94g2rrawphg0jfq6c1cmn6hegjd&rid=giphy.gif&ct=s&1625875200092" width="150" height="40" alt="play"/></a></td>
+</tr>
+
+<tr>
+<td>Smithereens</td>
+<td><a href="https://www.youtube.com/watch?v=lcpVMHd9LXk"><span class="blanco"></span>
+<img src="https://media1.giphy.com/media/coBIzlrDq93doYaA9U/giphy.gif?cid=a267dfa328yx6jb6prtte94g2rrawphg0jfq6c1cmn6hegjd&rid=giphy.gif&ct=s&1625875200092" width="150" height="40" alt="play"/></a></td>
+</tr>
+
+<tr>
+<td>Heavydirtysoul</td>
+<td><a href="https://www.youtube.com/watch?v=r_9Kf0D5BTs"><span class="blanco"></span>
+<img src="https://media1.giphy.com/media/coBIzlrDq93doYaA9U/giphy.gif?cid=a267dfa328yx6jb6prtte94g2rrawphg0jfq6c1cmn6hegjd&rid=giphy.gif&ct=s&1625875200092" width="150" height="40" alt="play"/></a></td>
+</tr>
+
+<tr>
+<td>My Blood</td>
+<td><a href="https://www.youtube.com/channel/UCBQZwaNPFfJ1gZ1fLZpAEGw"><span class="blanco"></span>
+<img src="https://media1.giphy.com/media/coBIzlrDq93doYaA9U/giphy.gif?cid=a267dfa328yx6jb6prtte94g2rrawphg0jfq6c1cmn6hegjd&rid=giphy.gif&ct=s&1625875200092" width="150" height="40" alt="play"/></a></td>
+</tr>
+
+<tr>
+<td>Migraine</td>
+<td><a href="https://www.youtube.com/watch?v=z-wYtUBBTuY"><span class="blanco"></span>
+<img src="https://media1.giphy.com/media/coBIzlrDq93doYaA9U/giphy.gif?cid=a267dfa328yx6jb6prtte94g2rrawphg0jfq6c1cmn6hegjd&rid=giphy.gif&ct=s&1625875200092" width="150" height="40" alt="play"/></a></td>
+</tr>
+
+<tr>
+<td>House of Gold</td>
+<td><a href="https://www.youtube.com/watch?v=mDyxykpYeu8"><span class="blanco"></span>
+<img src="https://media1.giphy.com/media/coBIzlrDq93doYaA9U/giphy.gif?cid=a267dfa328yx6jb6prtte94g2rrawphg0jfq6c1cmn6hegjd&rid=giphy.gif&ct=s&1625875200092" width="150" height="40" alt="play"/></a></td>
+</tr>
+
+<tr>
+<td>Jumpsuit</td>
+<td><a href="https://www.youtube.com/watch?v=UOUBW8bkjQ4"><span class="blanco"></span>
+<img src="https://media1.giphy.com/media/coBIzlrDq93doYaA9U/giphy.gif?cid=a267dfa328yx6jb6prtte94g2rrawphg0jfq6c1cmn6hegjd&rid=giphy.gif&ct=s&1625875200092" width="150" height="40" alt="play"/></a></td>
+</tr>
+
+<tr>
+<td>Nico and the niners</td>
+<td><a href="https://www.youtube.com/watch?v=hMAPyGoqQVw"><span class="blanco"></span>
+<img src="https://media1.giphy.com/media/coBIzlrDq93doYaA9U/giphy.gif?cid=a267dfa328yx6jb6prtte94g2rrawphg0jfq6c1cmn6hegjd&rid=giphy.gif&ct=s&1625875200092" width="150" height="40" alt="play"/></a></td>
+</tr>
+
+<tr>
+<td>We don´t believe whats on TV</td>
+<td><a href="https://www.youtube.com/watch?v=Xa9nJch8Ydk"><span class="blanco"></span>
+<img src="https://media1.giphy.com/media/coBIzlrDq93doYaA9U/giphy.gif?cid=a267dfa328yx6jb6prtte94g2rrawphg0jfq6c1cmn6hegjd&rid=giphy.gif&ct=s&1625875200092" width="150" height="40" alt="play"/></a></td>
+</tr>
+
+<tr>
+<td>Holding On to You</td>
+<td><a href="https://www.youtu.be/ltBMxkLUIwY"><span class="blanco"></span>
+<img src="https://media1.giphy.com/media/coBIzlrDq93doYaA9U/giphy.gif?cid=a267dfa328yx6jb6prtte94g2rrawphg0jfq6c1cmn6hegjd&rid=giphy.gif&ct=s&1625875200092" width="150" height="40" alt="play"/></a></td>
+</tr>
+
+<tr>
+<td>Lovely</td>
+<td><a href="https://www.youtube.com/watch?v=3C-2xCuJZYQ"><span class="blanco"></span>
+<img src="https://media1.giphy.com/media/coBIzlrDq93doYaA9U/giphy.gif?cid=a267dfa328yx6jb6prtte94g2rrawphg0jfq6c1cmn6hegjd&rid=giphy.gif&ct=s&1625875200092" width="150" height="40" alt="play"/></a></td>
+</tr>
+
+<tr>
+<td>Fake you Out</td>
+<td><a href="https://www.youtube.com/watch?v=2sn0ZSq8-Rc"><span class="blanco"></span>
+<img src="https://media1.giphy.com/media/coBIzlrDq93doYaA9U/giphy.gif?cid=a267dfa328yx6jb6prtte94g2rrawphg0jfq6c1cmn6hegjd&rid=giphy.gif&ct=s&1625875200092" width="150" height="40" alt="play"/></a></td>
+</tr>
+
+<tr>
+<td>Oh ms Believer </td>
+<td><a href="https://www.youtube.com/watch?v=IpiaYAGjXhU"><span class="blanco"></span>
+<img src="https://media1.giphy.com/media/coBIzlrDq93doYaA9U/giphy.gif?cid=a267dfa328yx6jb6prtte94g2rrawphg0jfq6c1cmn6hegjd&rid=giphy.gif&ct=s&1625875200092" width="150" height="40" alt="play"/></a></td>
+</tr>
+
+<tr>
+<td>Forest</td>
+<td><a href="https://www.youtube.com/watch?v=5daYd91k4ws"><span class="blanco"></span>
+<img src="https://media1.giphy.com/media/coBIzlrDq93doYaA9U/giphy.gif?cid=a267dfa328yx6jb6prtte94g2rrawphg0jfq6c1cmn6hegjd&rid=giphy.gif&ct=s&1625875200092" width="150" height="40" alt="play"/></a></td>
+</tr>
+
+<tr>
+<td>Legend</td>
+<td><a href="https://www.youtube.com/watch?v=f3bzqzspXPI"><span class="blanco"></span>
+<img src="https://media1.giphy.com/media/coBIzlrDq93doYaA9U/giphy.gif?cid=a267dfa328yx6jb6prtte94g2rrawphg0jfq6c1cmn6hegjd&rid=giphy.gif&ct=s&1625875200092" width="150" height="40" alt="play"/></a></td>
+</tr>
+
+<tr>
+<td>The Run and Go</td>
+<td><a href="https://www.youtube.com/watch?v=qTUc91b9mxI"><span class="blanco">Escuchar</span></a></td>
+</tr>
+</table>
+`;
